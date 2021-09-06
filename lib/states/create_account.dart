a84afbb91b431474cad5c5b36c98af3f6bdb8eee
kanjiz <kanjizm@gmail.com>
2021-09-06 12:49:40 +0700

    update

---
+++ lib/states/create_account.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class CreateAccoun extends StatefulWidget {
+  const CreateAccoun({ Key? key }) : super(key: key);
+
+  @override
+  _CreateAccounState createState() => _CreateAccounState();
+}
+
+class _CreateAccounState extends State<CreateAccoun> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
---
+++ lib/ultility/my_constan.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class MyConstant {
+  // Genernal 
+  static String appName = 'Shopping Mall';
+
+  // Route
+  static String routeAuthen = '/authen';
+  static String routeCreateAccount = '/creatAccount';
+  static String routeBuyerService = 'buyerService';
+  static String routeSalerService = 'salerService';
+  static String routeRiderService = 'riderService';
+
+  //image
+  static String image1 = 'images/image1.png';
+  static String image2 = 'images/image2.png';
+  static String image3 = 'images/image3.png';
+  static String image4 = 'images/image4.png';
+
+  //Color
+  static Color primary = Color(0xff87861d);
+  static Color dark = Color(0xff575900);
+  static Color light = Color(0xffb9b64e);
+  
+  // style
+  TextStyle h1Style() => TextStyle(
+    fontSize: 24,
+    color: dark,
+    fontWeight: FontWeight.bold,
+  );
+  TextStyle h2Style() => TextStyle(
+    fontSize: 18,
+    color: dark,
+    fontWeight: FontWeight.w700,
+  );
+  TextStyle h3Style() => TextStyle(
+    fontSize: 14,
+    color: dark,
+    fontWeight: FontWeight.normal,
+  );
+
+}
